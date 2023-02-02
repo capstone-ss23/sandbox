aa9e5f580f1a1f2bd35a093dc2968eca58db1539
rodsan0 <rodsan0@users.noreply.github.com>
2023-02-02 01:31:43 -0500

    Implement pressure support

--- santiago/canvas-sandbox/canvas_handler.js
+++ santiago/canvas-sandbox/canvas_handler.js
@@ -2,6 +2,7 @@ var ispointerDown = false;
 var pointerDownX = 0;
 var pointerDownY = 0;
 var line_width = 0;
+var pressureEnabled = false;
 
 var hue = 0;
 var saturation = 100;
@@ -46,6 +47,10 @@ function cssColor(h, s, l) {
         .replace(/L/, l);
 }
 
+function togglePressure() {
+    pressureEnabled = !pressureEnabled;
+}
+
 function pointerDown(event) {
     x = event.clientX;
     y = event.clientY;
@@ -56,7 +61,7 @@ function pointerDown(event) {
 
     // draw circle at pointer
     ctx.beginPath();
-    ctx.arc(x, y, line_width, 0, 2 * Math.PI, false);
+    ctx.arc(x, y, getLineWidth(event), 0, 2 * Math.PI, false);
     ctx.fillStyle = cssColor(hue, saturation, lightness);
     ctx.fill();
 
@@ -65,6 +70,15 @@ function pointerDown(event) {
     pointerDownX = x;
     pointerDownY = y;
 }
+
+function getLineWidth(event) {
+    switch (event.pointerType) {
+        case 'mouse':
+            return line_width;
+        case 'pen':
+        case 'touch':
+            return pressureEnabled ? line_width * event.pressure : line_width;
+    }}
 
 function pointerUp(event) {
     ispointerDown = false;
@@ -85,14 +99,14 @@ function pointerMove(event) {
         ctx.beginPath();
         ctx.moveTo(pointerDownX, pointerDownY);
         ctx.lineTo(x, y);
-        ctx.lineWidth = 2 * line_width;
+        ctx.lineWidth = 2 * getLineWidth(event);
         ctx.strokeStyle = cssColor(hue, saturation, lightness);
         ctx.fillStyle = cssColor(hue, saturation, lightness);
         ctx.stroke();
 
         // draw circle at end of pointer
         ctx.beginPath();
-        ctx.arc(x, y, line_width, 0, 2 * Math.PI, false);
+        ctx.arc(x, y, getLineWidth(event), 0, 2 * Math.PI, false);
         ctx.fillStyle = cssColor(hue, saturation, lightness);
         ctx.fill();
 
